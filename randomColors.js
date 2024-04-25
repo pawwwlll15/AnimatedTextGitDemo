d55d81dd42b0190d007a1fee4eff41d1886bdba7
Paul Mozeley <pdreier15@gmail.com>
2024-04-25 11:56:45 -0400

    changing color scheme and timer

--- randomColors.js
+++ randomColors.js
@@ -1,8 +1,8 @@
 /* creating random colors, but only shades of reds */
 function getRandomColor(){
     const r = Math.floor(Math.random()*256);
-    const g = Math.floor(Math.random()*5);
-    const b = Math.floor(Math.random()*20);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
 
     /* note the paranths(top left corner of keyboard) to create temperant literal string*/
     return `rgb(${r},${g},${b})`;
@@ -24,5 +24,5 @@ setInterval(function(){
     for(let letter of letters){
         letter.style.color = getRandomColor();
     }
-},1000);
+},500);
 
